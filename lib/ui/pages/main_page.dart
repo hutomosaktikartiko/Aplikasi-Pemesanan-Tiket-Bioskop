4a2a6fdffc6fcf00195033a9506aea6e948d9717
Hutomo <hutomosaktikartiko.siskom@gmail.com>
2020-06-11 23:29:23 +0700

    Custom Bottom NavBar

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -9,23 +9,68 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Main page")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<UserBloc, UserState>(
-                builder: (_, userState) => (userState is UserLoaded)
-                    ? Text(userState.user.name)
-                    : SizedBox()),
-            RaisedButton(
-                child: Text("Sign out"),
-                onPressed: () {
-                  AuthServices.singOut();
-                }),
-          ],
-        ),
-      ),
-    );
+        body: Stack(
+      children: <Widget>[
+        Container(color: accentColor1),
+        SafeArea(
+            child: Container(
+          color: Color(0xFFF6F7F9),
+        )),
+        ListView(),
+        createCustomBottomNavBar(),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 46,
+            width: 46,
+            margin: EdgeInsets.only(bottom: 42),
+            child: FloatingActionButton(
+              elevation: 0,
+              backgroundColor: accentColor2,
+              child: SizedBox(
+                height: 26,
+                width: 26,
+                child: Icon(MdiIcons.walletPlus, color: Colors.black.withOpacity(0.54),)
+              ),
+              onPressed: (){}),
+          ),
+        )
+      ],
+    ));
   }
+
+  Widget createCustomBottomNavBar() => Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipPath(
+          clipper: BottomNavBarClipper(),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20))),
+          ),
+        ),
+      );
+}
+
+class BottomNavBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(size.width / 2 - 28, 0);
+    path.quadraticBezierTo(size.width / 2 - 28, 33, size.width / 2, 33);
+    path.quadraticBezierTo(size.width / 2 + 28, 33, size.width / 2 + 28, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
