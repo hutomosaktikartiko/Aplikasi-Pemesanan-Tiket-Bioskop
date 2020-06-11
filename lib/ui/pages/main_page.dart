9564778da3d96197fbfb7e81bd350f46004b73d3
Hutomo <hutomosaktikartiko.siskom@gmail.com>
2020-06-11 23:43:18 +0700

    Main Page Nav

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -6,6 +6,16 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int bottomNavBarIndex;
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    bottomNavBarIndex = 0;
+    pageController = PageController(initialPage: bottomNavBarIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +26,22 @@ class _MainPageState extends State<MainPage> {
             child: Container(
           color: Color(0xFFF6F7F9),
         )),
-        ListView(),
+        PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              bottomNavBarIndex = index;
+            });
+          },
+          children: <Widget>[
+            Center(
+              child: Text("New Movie"),
+            ),
+            Center(
+              child: Text("My Ticket"),
+            ),
+          ],
+        ),
         createCustomBottomNavBar(),
         Align(
           alignment: Alignment.bottomCenter,
@@ -25,14 +50,16 @@ class _MainPageState extends State<MainPage> {
             width: 46,
             margin: EdgeInsets.only(bottom: 42),
             child: FloatingActionButton(
-              elevation: 0,
-              backgroundColor: accentColor2,
-              child: SizedBox(
-                height: 26,
-                width: 26,
-                child: Icon(MdiIcons.walletPlus, color: Colors.black.withOpacity(0.54),)
-              ),
-              onPressed: (){}),
+                elevation: 0,
+                backgroundColor: accentColor2,
+                child: SizedBox(
+                    height: 26,
+                    width: 26,
+                    child: Icon(
+                      MdiIcons.walletPlus,
+                      color: Colors.black.withOpacity(0.54),
+                    )),
+                onPressed: () {}),
           ),
         )
       ],
@@ -50,6 +77,47 @@ class _MainPageState extends State<MainPage> {
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20),
                     topRight: Radius.circular(20))),
+            child: BottomNavigationBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                selectedItemColor: mainColor,
+                unselectedItemColor: Color(0xFFE5E5E5),
+                currentIndex: bottomNavBarIndex,
+                onTap: (index) {
+                  setState(() {
+                    bottomNavBarIndex = index;
+                    //ketika di tab, page juga pindah
+                    pageController.jumpToPage(index);
+                  });
+                },
+                items: [
+                  BottomNavigationBarItem(
+                      title: Text(
+                        "New Movies",
+                        style: GoogleFonts.raleway(
+                            fontSize: 13, fontWeight: FontWeight.w600),
+                      ),
+                      icon: Container(
+                        margin: EdgeInsets.only(bottom: 6),
+                        height: 20,
+                        child: Image.asset((bottomNavBarIndex == 0)
+                            ? "assets/ic_movie.png"
+                            : "assets/ic_movie_grey.png"),
+                      )),
+                  BottomNavigationBarItem(
+                      title: Text(
+                        "My Tickets",
+                        style: GoogleFonts.raleway(
+                            fontSize: 13, fontWeight: FontWeight.w600),
+                      ),
+                      icon: Container(
+                        margin: EdgeInsets.only(bottom: 6),
+                        height: 20,
+                        child: Image.asset((bottomNavBarIndex == 1)
+                            ? "assets/ic_tickets.png"
+                            : "assets/ic_tickets_grey.png"),
+                      )),
+                ]),
           ),
         ),
       );
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
