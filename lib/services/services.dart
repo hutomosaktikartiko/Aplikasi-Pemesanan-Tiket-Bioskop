229d9e51f20e3fb7331d0bb10daa444d3237a4f8
unknown <hutomosaktikartiko.siskom@gmail.com>
2020-07-01 01:09:18 +0700

    Ticket Services

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -10,3 +10,4 @@ import 'package:http/http.dart' as http;
 part 'auth_services.dart';
 part 'user_services.dart';
 part 'movie_services.dart';
+part 'ticket_services.dart';
---
+++ lib/services/ticket_services.dart
@@ -0,0 +1,41 @@
+part of 'services.dart';
+
+class TicketServices {
+  static CollectionReference ticketCollection =
+      Firestore.instance.collection('tickets');
+
+  static Future<void> saveTicket(String id, Ticket ticket) async {
+    await ticketCollection.document().setData({
+      'movieID': ticket.movieDetail.id ?? "",
+      'userID': id ?? "",
+      'theaterName': ticket.theater.name ?? 0,
+      'time': ticket.time.millisecondsSinceEpoch ??
+          DateTime.now().millisecondsSinceEpoch,
+      'seats': ticket.seatsInString,
+      'name': ticket.name,
+      'totalPrice': ticket.totalPrice
+    });
+  }
+
+  static Future<List<Ticket>> getTickets(String userId) async {
+    QuerySnapshot snapshot = await ticketCollection.getDocuments();
+    var documents = snapshot.documents
+        .where((document) => document.data['userID'] == userId);
+
+    List<Ticket> tickets = [];
+    for (var document in documents) {
+      MovieDetail movieDetail =
+          await MovieServices.getDetails(document.data['movieID']);
+      tickets.add(Ticket(
+          movieDetail,
+          Theater(document.data['theaterName']),
+          DateTime.fromMillisecondsSinceEpoch(document.data['time']),
+          document.data['bookingCode'],
+          document.data['seats'].toString().split(','),
+          document.data['name'],
+          document.data['totallPrice']));
+    }
+
+    return tickets;
+  }
+}
